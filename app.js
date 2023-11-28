bed191d136c410af565c0889e71108acb02aef52
Rohama-Majeed7 <139218403+Rohama-Majeed7@users.noreply.github.com>
2023-11-28 21:44:06 +0500

    change

---
+++ app.js
@@ -0,0 +1,5 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+const show = document.querySelector(".shownav");
+function icon() {
+  show.classList.toggle("nav");
+}
